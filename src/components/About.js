9d9003b9baa71d567ba6bb312cee07a0ccc68ec4
dmimar382 <dmimar382@gmail.com>
2024-07-25 20:15:07 -0500

    aboutme & formatting

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import profileImage from '../styles/images/about/profile_image_1.jpg';
+import aboutData from '../styles/download/aboutData.json'; 
 
 const About = () => {
+    const descriptionArray = aboutData.about_me?.description || [];
     return (
         <section id="about" className="about">
             <div className="section-heading text-center">
@@ -12,19 +14,14 @@ const About = () => {
                     <div className="row">
                         <div className="col-sm-offset-1 col-sm-5">
                             <div className="single-about-img">
-                                <img src={profileImage}  alt="profile_image" />
+                                <img src={profileImage} alt="profile_image" />
                             </div>
                         </div>
                         <div className="col-sm-6">
                             <div className="single-about-txt">
-                                <h3>
-                                    I'm a software developer who finds joy in combining creativity with practicality. With a Bachelor's in Computer Engineering from Texas A&M University and a backend software development certification from Codecademy, I've honed my skills in software development.
-                                </h3>
-                                <p>
-                                    My approach to development is rooted in problem understanding and a keen sense of organization. Although I'm relatively new to the field, my background as an electrical engineer equips me with a detail-oriented mindset.
-                                    <br /><br />
-                                    While my portfolio is in its early stages, I'm excited to embark on this journey, aiming to create efficient and user-friendly software solutions. Thank you for visiting, and I look forward to sharing my growth and future projects with you.
-                                </p>
+                            {descriptionArray.map((paragraph, index) => (
+                                    <p key={index}>{paragraph}</p>
+                                ))}
                             </div>
                         </div>
                     </div>
